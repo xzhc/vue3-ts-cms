8fd682bda28d6ac0a59b97cffcf74e3c940ada24
xzhc <1838998336@qq.com>
2024-06-17 23:42:31 +0800

    集成element-plus

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import './assets/css/index.less'
 import router from './router'
 import pinia from './store'
 
+//1.全局引入element-plus:方便简洁但是打包后文件大
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// app.use(ElementPlus)
+
+//2.按需求引入
+
 const app = createApp(App)
+
 app.use(router)
 app.use(pinia)
 app.mount('#app')
