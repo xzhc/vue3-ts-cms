0d984254bbb23501c550cfa85b6c4954ca931e09
xzhc <1838998336@qq.com>
2024-06-23 23:41:28 +0800

    调整代码格式和更新依赖项
    - 将printWidth设置为80以适应更紧凑的代码风格。
    - 添加了新的自动导入声明和组件声明，以整合element-plus库。
    - 更新了package.json和pnpm-lock.yaml，增加了新的依赖项和更新了现有依赖项的版本。
    - 删除了不必要的空白行和注释，以保持代码的整洁。
    - 按照最新的代码规范和样式进行了代码格式化。

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createStyleImportPlugin, ElementPlusResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,19 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name: string) => {
+            name = name.substring(3, name.length)
+            return `element-plus/es/components/${name}/style/index`
+          }
+        }
+      ]
     })
   ],
   resolve: {
